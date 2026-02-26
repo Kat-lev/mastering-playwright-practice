0a6875ed5acbcb9a8012701584348a15ad412520
kat-lev <kat@katleverton.com>
2026-02-26 13:42:54 +0100

    Hotfix: typo in file name

--- tests/auth.setup.ts
+++ tests/auth.setup.ts
@@ -3,7 +3,7 @@ import { test as setup, expect} from "@playwright/test";
 setup("Create customer 01 auth", async ({ page, context }) => {
   const email = "customer@practicesoftwaretesting.com";
   const password = "welcome01";
-  const customer01AuthFile = ".auth/customer01-json";
+  const customer01AuthFile = ".auth/customer01.json";
 
   await page.goto("https://practicesoftwaretesting.com/auth/login")
   //Fill email using const above
