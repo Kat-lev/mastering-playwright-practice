8600522d42af91c5971b63d681b147c4e3dfaa86
kat-lev <kat@katleverton.com>
2026-02-26 12:48:43 +0100

    Add beforeEach block to homepage tests

--- tests/home.spec.ts
+++ tests/home.spec.ts
@@ -1,31 +1,28 @@
 import { test, expect } from "@playwright/test";
 
 test.describe("Home page", () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto("https://practicesoftwaretesting.com/");
+  })
 
   test("Check sign-in is displayed", async ({ page }) => {
-    await page.goto("https://practicesoftwaretesting.com/");
     //Test from video
     await expect(page.getByTestId("nav-sign-in")).toHaveText("Sign in");
   });
 
   test("Validate page title", async ({ page }) => {
-    await page.goto("https://practicesoftwaretesting.com/");
     //Test from video
     await expect(page).toHaveTitle("Practice Software Testing - Toolshop - v5.0",);
   });
 
   test("Check count of items displayed", async ({ page }) => {
-    await page.goto("https://practicesoftwaretesting.com/");
     //Test from video
     const productGrid = page.locator(".col-md-9");
     await expect(productGrid.getByRole("link")).toHaveCount(9);
   })
 
-  test("Home page test challenge - find and check Thor Hammer", async ({
-    page,
-  }) => {
+  test("Home page test challenge - find and check Thor Hammer", async ({ page }) => {
     //Challenge: Search for Thor Hammer and check result
-    await page.goto("https://practicesoftwaretesting.com/");
     //Locate search bar & fill Thor
     await page.getByTestId("search-query").fill("Thor");
     //Submit search
