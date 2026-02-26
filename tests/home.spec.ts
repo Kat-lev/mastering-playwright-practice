eb8868658ae5404ae1b15c6e9252eecd6ddb7f9e
kat-lev <kat@katleverton.com>
2026-02-26 16:56:50 +0100

    Add visual screenshot capture to home.spec.ts tests

--- tests/home.spec.ts
+++ tests/home.spec.ts
@@ -4,23 +4,28 @@ test.describe("Home page with no auth", () => {
   test.beforeEach(async ({ page }) => {
     await page.goto("https://practicesoftwaretesting.com/");
   });
-  test("check sign in", async ({ page }) => {
+
+  test("Check screenshot - no auth", async ({ page }) => {
+    await expect(page).toHaveScreenshot("home-no-auth.png");
+  }) 
+
+  test("Check sign in", async ({ page }) => {
     await expect(page.getByTestId("nav-sign-in")).toHaveText("Sign in");
   });
 
-  test("validate page title", async ({ page }) => {
+  test("Validate page title", async ({ page }) => {
     await expect(page).toHaveTitle(
       "Practice Software Testing - Toolshop - v5.0"
     );
   });
 
-  test("grid loads with 9 items", async ({ page }) => {
+  test("Grid loads with 9 items", async ({ page }) => {
     const productGrid = page.locator(".col-md-9");
     await expect(productGrid.getByRole("link")).toHaveCount(9);
     expect(await productGrid.getByRole("link").count()).toBe(9);
   });
 
-  test("search for Thor Hammer", async ({ page }) => {
+  test("Search for Thor Hammer", async ({ page }) => {
     const productGrid = page.locator(".col-md-9");
     await page.getByTestId("search-query").fill("Thor Hammer");
     await page.getByTestId("search-submit").click();
@@ -34,7 +39,12 @@ test.describe("Home page cusotmer 01 auth", () => {
   test.beforeEach(async ({ page }) => {
     await page.goto("https://practicesoftwaretesting.com/");
   })
-  test("Check cust01 is signed in correctly", async ({ page }) => {
+
+  test("Check screenshot - customer01 auth", async ({ page }) => {
+    await expect(page).toHaveScreenshot("home-cust01-auth.png");
+  }); 
+
+  test("Check customer01 is signed in correctly", async ({ page }) => {
     //Sign-in text should be visible as user should already be signed in. Use ".not" syntax.
     await expect(page.getByTestId("nav-sign-in")).not.toBeVisible();
     await expect(page.locator('[data-test="nav-menu"]')).toContainText("Jane Doe");
