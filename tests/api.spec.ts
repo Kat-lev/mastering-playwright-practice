38d7dbee1503f04dfac0b4f1e4af944630968c84
kat-lev <kat@katleverton.com>
2026-02-27 12:36:00 +0100

    Create login auth API test

--- tests/api.spec.ts
+++ tests/api.spec.ts
@@ -16,3 +16,20 @@ test("GET /products test", async ({ request }) => {
   //There are 50 total products
   expect(body.total).toBe(50);
 });
+
+//New test: user login POST req to users/login
+test("check POST /users/login", async ({ request}) => {
+  const apiUrl = "https://api.practicesoftwaretesting.com";
+  const response = await request.post(apiUrl + "/users/login", {
+    //Data is entered here - it's the payload
+    data:{
+      email: "customer@practicesoftwaretesting.com",
+      password: "welcome01",
+    },
+  });
+  expect (response.status()).toBe(200);
+  const body = await response.json();
+  //console.log(body);
+  //This console long (viewed in debugger) shows us the auth, access token and type
+  expect(body.access_token).toBeTruthy();
+})
